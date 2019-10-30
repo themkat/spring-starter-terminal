823eb10db1ef9919ffb5f5bcc1fd87f77e156728
Marie Katrine Ekeberg <spacemarky@gmail.com>
2019-10-30 21:48:07 +0100

    Now possible to edit various settings (including artifact info and dependency list) and create the project (unpack to disc). Should probably work on this project more often than once in a blue moon :P

--- spring-starter.sh
+++ spring-starter.sh
@@ -9,19 +9,33 @@ JAVA_VERSION=$(echo $STARTER_METADATA | jq -r '.javaVersion.default')
 GROUP_ID=$(echo $STARTER_METADATA | jq -r '.groupId.default')
 ARTIFACT_ID=$(echo $STARTER_METADATA | jq -r '.artifactId.default')
 DESCRIPTION=$(echo $STARTER_METADATA | jq -r '.description.default')
-
+DEPENDENCIES=""
 
 function artifactSettings () {
 	while [ 1 ]
 	do
-		ACTION=$(dialog --stdout --backtitle 'Spring Initializer Terminal Edition' --inputmenu "Artifact settings" 0 0 10 "Group id:" "$GROUP_ID" "Artifact id:" "$ARTIFACT_ID" "Description:" "$DESCRIPTION")
-
-		if [ $? -ne 0]
+		ACTION=$(dialog --stdout --backtitle 'Spring Initializer Terminal Edition' --no-cancel --inputmenu "Artifact settings" 0 0 10 "Group id:" "$GROUP_ID" "Artifact id:" "$ARTIFACT_ID" "Description:" "$DESCRIPTION")
+		
+		# not renaming, so OK was selected. gtf back to the menu
+		if [ -z $(echo $ACTION | grep RENAMED)]
 		then
 			break
 		fi
 
-		# TODO: finish it. See if we get RENAMED something back.
+		# TODO: refactor. Stupid solution :P
+		SELECTIONS=$(echo ${ACTION:8} | sed 's/: /:+/g')
+		SELECTED_TYPE=$(echo $SELECTIONS | cut -d + -f1)
+		SELECTED_VALUE=$(echo $SELECTIONS | cut -d + -f2)
+	    case $SELECTED_TYPE in
+			"Group id:")
+				GROUP_ID=$SELECTED_VALUE
+				;;
+			"Artifact id:")
+				ARTIFACT_ID=$SELECTED_VALUE
+				;;
+			"Description:")
+				DESCRIPTION=$SELECTED_VALUE
+		esac
 	done
 	
 }
@@ -35,13 +49,13 @@ function dependencyManagement () {
 	# TODO: how should descriptions be viewed?
 	# TODO: make the choices "persistent" between "saves"
 	DEPENDENCY_LIST=$(echo $STARTER_METADATA | jq '.dependencies.values | map(.values[]) | map(.id, .name)[]' | sed '0~2 s/$/\noff/g' | tr '\n' ' ')
-	eval "dialog --stdout --backtitle \"Spring Initializer Terminal Edition\" --checklist \"Choose dependencies\" 0 0 0 $DEPENDENCY_LIST"
+	DEPENDENCIES=$(eval "dialog --stdout --backtitle \"Spring Initializer Terminal Edition\" --checklist \"Choose dependencies\" 0 0 0 $DEPENDENCY_LIST" | sed 's/ /,/g')
 }
 
 
 function changeLanguage () {
 	LANGUAGE_SELECTIONS=$(echo $STARTER_METADATA | jq '.language.values | map(.id, .name)[]')
-    eval "dialog --stdout --backtitle 'Spring Initializer Terminal Edition' --radiolist 'Select language' 0 0 0 $(echo "$LANGUAGE_SELECTIONS" | sed '0~2 s/$/ off/g' | tr '\n' ' ')"
+    LANGUAGE=$(eval "dialog --stdout --backtitle 'Spring Initializer Terminal Edition' --radiolist 'Select language' 0 0 0 $(echo "$LANGUAGE_SELECTIONS" | sed '0~2 s/$/ off/g' | tr '\n' ' ')")
 }
 
 
@@ -51,9 +65,8 @@ function changeJavaVersion () {
 	JAVA_VERSION_SELECTIONS=$(echo $STARTER_METADATA | jq '.javaVersion.values | map(.id, .name)[]' | sed '0~2 s/^\"/\"Java /g')
 	# TODO: find a better way to determine the last number
 	NUM_JAVA_VERSIONS=$(expr $(echo "$JAVA_VERSION_SELECTIONS" | wc -l) / 2)
-	eval "dialog --stdout --backtitle 'Spring Initializer Terminal Edition' --radiolist 'Select Java version' 0 0 $NUM_JAVA_VERSIONS $(echo "$JAVA_VERSION_SELECTIONS" | sed '0~2 s/$/ off/g' | tr '\n' ' ')"
+	JAVA_VERSION=$(eval "dialog --stdout --backtitle 'Spring Initializer Terminal Edition' --radiolist 'Select Java version' 0 0 $NUM_JAVA_VERSIONS $(echo "$JAVA_VERSION_SELECTIONS" | sed '0~2 s/$/ off/g' | tr '\n' ' ')")
 }
-
 
 
 # flow:
@@ -69,7 +82,7 @@ function changeJavaVersion () {
 
 while [ 1 ]
 do
-	CHOICE=$(dialog --stdout --backtitle 'Spring Initializer Terminal Edition' --title "Select option" --menu "Group id:            $GROUP_ID\nArtifact id:         $ARTIFACT_ID\n\nBuild tool:          $BUILD_TOOL\nLanguage:            $LANGUAGE\nSpring Boot version: $SPRING_BOOT_VERSION\nJava version:        $JAVA_VERSION\n" 0 0 0 "a" "Artifact settings" "p" "Change project information" "l" "Change language" "j" "Change Java version" "d" "Manage dependencies" "c" "Create project")
+	CHOICE=$(dialog --stdout --backtitle 'Spring Initializer Terminal Edition' --title "Select option" --menu "Artifact information:\nGroup id:            $GROUP_ID\nArtifact id:         $ARTIFACT_ID\n\nBuild tool:          $BUILD_TOOL\nLanguage:            $LANGUAGE\nSpring Boot version: $SPRING_BOOT_VERSION\nJava version:        $JAVA_VERSION\n" 0 0 0 "a" "Artifact settings" "p" "Change project information" "l" "Change language" "j" "Change Java version" "d" "Manage dependencies" "c" "Create project")
 
 	if [ $? -ne 0 ]
 	then 
@@ -89,6 +102,21 @@ do
 			;;
 		"d")
 			dependencyManagement
+			;;
+		"c")
+			# If network issues then we get an empty folder... "Neat" :P 
+			mkdir $ARTIFACT_ID 
+			curl -s https://start.spring.io/starter.tgz -d language=$LANGUAGE \
+				 -d javaVersion=$JAVA_VERSION \
+				 -d type=$BUILD_TOOL \
+				 -d bootVersion=$SPRING_BOOT_VERSION \
+				 -d groupId=$GROUP_ID \
+				 -d artifactId=$ARTIFACT_ID \
+				 -d description=$DESCRIPTION \
+				 -d dependencies=$DEPENDENCIES \
+				| tar --directory $ARTIFACT_ID -zxf - && \
+				echo "Spring Boot project now in $ARTIFACT_ID directory... Happy Hacking! :)"
+			exit 0
 			;;
 	esac
 done
